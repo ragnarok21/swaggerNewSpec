9ccf0d4478877111a3bb767505a4fcb40c20b16d
ragnarok21 <milan22>
2019-10-09 00:46:29 -0300

    se crea swagger new spec

---
+++ src/main/java/com/example/demo/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        registry.addResourceHandler("/json/**")
+                .addResourceLocations("classpath:/json/");
+    }
+}
